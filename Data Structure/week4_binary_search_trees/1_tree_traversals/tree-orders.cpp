bfce5981c994a9c42420b46f304be7ebf3d36476
Abhishek Kaushal <akaushal123@gmail.com>
2020-08-04 12:44:29 +0530

    Add files via upload

---
+++ Data Structure/week4_binary_search_trees/1_tree_traversals/tree-orders.cpp
@@ -0,0 +1,145 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#if defined(__unix__) || defined(__APPLE__)
+#include <sys/resource.h>
+#endif
+
+using std::vector;
+using std::ios_base;
+using std::cin;
+using std::cout;
+
+class TreeOrders {
+  int n;
+  vector <int> key;
+  vector <int> left;
+  vector <int> right;
+
+public:
+  void read() {
+    cin >> n;
+    key.resize(n);
+    left.resize(n);
+    right.resize(n);
+    for (int i = 0; i < n; i++) {
+      cin >> key[i] >> left[i] >> right[i];
+    }
+  }
+
+  void inorder_helper(vector<int> &result, int index){
+    if(index == -1)
+        return ;
+
+    int k = key[index];
+    int l = left[index];
+    int r = right[index];
+
+    inorder_helper(result, l);
+    result.push_back(k);
+    inorder_helper(result, r);
+  }
+
+  vector <int> in_order() {
+    vector<int> result;
+
+    if(key.empty())
+        return result;
+
+    inorder_helper(result, 0);
+    return result;
+  }
+
+  void preorder_helper(vector<int> &result, int index){
+    if(index == -1)
+        return ;
+
+    int k = key[index];
+    int l = left[index];
+    int r = right[index];
+
+    result.push_back(k);
+    preorder_helper(result, l);
+    preorder_helper(result, r);
+  }
+
+  vector <int> pre_order() {
+    vector<int> result;
+    if(key.empty())
+        return result;
+
+    preorder_helper(result, 0);
+
+    return result;
+  }
+
+  void postorder_helper(vector<int> &result, int index){
+    if(index == -1)
+        return ;
+
+    int k = key[index];
+    int l = left[index];
+    int r = right[index];
+
+    postorder_helper(result, l);
+    postorder_helper(result, r);
+    result.push_back(k);
+  }
+
+  vector <int> post_order() {
+    vector<int> result;
+    if(key.empty())
+        return result;
+
+    postorder_helper(result, 0);
+
+    return result;
+  }
+};
+
+void print(vector <int> a) {
+  for (size_t i = 0; i < a.size(); i++) {
+    if (i > 0) {
+      cout << ' ';
+    }
+    cout << a[i];
+  }
+  cout << '\n';
+}
+
+int main_with_large_stack_space() {
+  ios_base::sync_with_stdio(0);
+  TreeOrders t;
+  t.read();
+  print(t.in_order());
+  print(t.pre_order());
+  print(t.post_order());
+  return 0;
+}
+
+int main (int argc, char **argv)
+{
+#if defined(__unix__) || defined(__APPLE__)
+  // Allow larger stack space
+  const rlim_t kStackSize = 16 * 1024 * 1024;   // min stack size = 16 MB
+  struct rlimit rl;
+  int result;
+
+  result = getrlimit(RLIMIT_STACK, &rl);
+  if (result == 0)
+  {
+      if (rl.rlim_cur < kStackSize)
+      {
+          rl.rlim_cur = kStackSize;
+          result = setrlimit(RLIMIT_STACK, &rl);
+          if (result != 0)
+          {
+              std::cerr << "setrlimit returned result = " << result << std::endl;
+          }
+      }
+  }
+#endif
+
+  return main_with_large_stack_space();
+}
+
---
+++ Data Structure/week4_binary_search_trees/3_is_bst_advanced/is_bst_hard.cpp
@@ -0,0 +1,59 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <limits>
+#include <stdint.h>
+#include <climits>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::vector;
+
+struct Node {
+  int key;
+  int left;
+  int right;
+
+  Node() : key(0), left(-1), right(-1) {}
+  Node(int key_, int left_, int right_) : key(key_), left(left_), right(right_) {}
+};
+
+bool isBST(const vector<Node>& tree, int index, long min, long max){
+
+    if(index == -1)
+        return true;
+
+    long key = tree[index].key;
+    long left_index = tree[index].left;
+    long right_index = tree[index].right;
+
+    if(key < min || key > max)
+        return false;
+
+    return isBST(tree, left_index, min, key - 1) && isBST(tree, right_index, key, max);
+}
+
+bool IsBinarySearchTree(const vector<Node>& tree) {
+  // Implement correct algorithm here
+  if(tree.empty()) return true;
+
+  return isBST(tree, 0, LONG_MIN, LONG_MAX);
+}
+
+int main() {
+  int nodes;
+  cin >> nodes;
+  vector<Node> tree;
+  for (int i = 0; i < nodes; ++i) {
+    int key, left, right;
+    cin >> key >> left >> right;
+    tree.push_back(Node(key, left, right));
+  }
+  if (IsBinarySearchTree(tree)) {
+    cout << "CORRECT" << endl;
+  } else {
+    cout << "INCORRECT" << endl;
+  }
+  return 0;
+}
