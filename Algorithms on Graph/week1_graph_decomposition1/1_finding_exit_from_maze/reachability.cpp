b6d43cd4d37974a3af7cc4711ff1c844d92dd895
Abhishek Kaushal <akaushal123@gmail.com>
2020-08-05 14:16:47 +0530

    Add files via upload

---
+++ Algorithms on Graph/week1_graph_decomposition1/1_finding_exit_from_maze/reachability.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+using std::vector;
+using std::pair;
+
+int explore(vector<vector<int> > &adj, int x, int y, vector<bool> &visited){
+    if (x == y) return 1;
+    visited[x] = true;
+    for(auto w: adj[x]){
+        if(!visited[w])
+            if(explore(adj, w, y, visited))
+                return 1;
+    }
+    return 0;
+}
+
+int reach(vector<vector<int> > &adj, int x, int y) {
+  //write your code here
+  vector<bool> visited(adj.size(), false);
+  return explore(adj, x, y, visited);
+}
+
+int main() {
+  size_t n, m;
+  std::cin >> n >> m;
+  vector<vector<int> > adj(n, vector<int>());
+  for (size_t i = 0; i < m; i++) {
+    int x, y;
+    std::cin >> x >> y;
+    adj[x - 1].push_back(y - 1);
+    adj[y - 1].push_back(x - 1);
+  }
+  int x, y;
+  std::cin >> x >> y;
+  std::cout << reach(adj, x - 1, y - 1);
+}
---
+++ Algorithms on Graph/week1_graph_decomposition1/2_adding_exits_to_maze/connected_components.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+using std::vector;
+using std::pair;
+
+void explore_connected_components(vector<vector<int> > &adj, int v, vector<bool> &visited){
+    visited[v] = true;
+    for(auto w: adj[v])
+        if(!visited[w])
+            explore_connected_components(adj, w, visited);
+}
+
+int number_of_components(vector<vector<int> > &adj) {
+  int res = 0;
+  //write your code here
+  vector<bool> visited(adj.size(), false);
+
+  for(int v = 0; v < adj.size(); v++){
+    if(!visited[v]){
+        explore_connected_components(adj, v, visited);
+        res += 1;
+    }
+  }
+  return res;
+}
+
+int main() {
+  size_t n, m;
+  std::cin >> n >> m;
+  vector<vector<int> > adj(n, vector<int>());
+  for (size_t i = 0; i < m; i++) {
+    int x, y;
+    std::cin >> x >> y;
+    adj[x - 1].push_back(y - 1);
+    adj[y - 1].push_back(x - 1);
+  }
+  std::cout << number_of_components(adj);
+}
